b935abe81dc9d2439cfdd860d195c0ca09dcb210
TrevorRezac <trev.rezac@gmail.com>
2021-10-07 13:11:39 -0700

    working add calculator

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ import { add } from '../calculations.js';
 
 console.log(add(50, 100));
 // reference needed DOM elements
-
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
 // set event listeners 
+
+addButton.addEventListener('click', () => {
     // get user input(s)
+    const x = +addNumber1.value;
+    const y = +addNumber2.value;
+
     // do any needed work with the value(s)
+
+    const sum = add(x, y);
+
     // update DOM to reflect new value(s)
+    addAnswer.textContent = sum;
+
+});
