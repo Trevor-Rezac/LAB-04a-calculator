8646814b9a9ce7b7a3cbcd365a5aa2cf503b5ad8
TrevorRezac <trev.rezac@gmail.com>
2021-10-07 13:57:47 -0700

    working subtraction calculator

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // import functions
 import { add } from '../calculations.js';
+import { subtract } from '../calculations.js';
 
 console.log(add(50, 100));
+console.log(subtract(100, 50));
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
+
+const subNumber1 = document.getElementById('sub-number-1');
+const subNumber2 = document.getElementById('sub-number-2');
+const subButton = document.getElementById('sub-button');
+const subAnswer = document.getElementById('sub-answer');
 
 // set event listeners 
 
@@ -22,4 +29,14 @@ addButton.addEventListener('click', () => {
     // update DOM to reflect new value(s)
     addAnswer.textContent = sum;
 
+});
+
+subButton.addEventListener('click', () => {
+
+    const x = +subNumber1.value;
+    const y = +subNumber2.value;
+
+    const diff = subtract(x, y);
+
+    subAnswer.textContent = diff;
 });
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 import { add } from '../calculations.js';
+import { subtract } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -11,7 +12,6 @@ test('adds two numbers 5 and 3', (expect) => {
     const y = 3;
     const expected = 8;
 
-    
     //2. Act 
     // Call the function you're testing and set the result to a const
     const actual = add(x, y); // use your function here
@@ -21,17 +21,17 @@ test('adds two numbers 5 and 3', (expect) => {
     expect.equal(actual, expected);
 });
 
-test('adds two numbers 5 and 3', (expect) => {
+test('subtracts two numbers 11 and 5', (expect) => {
     //1. Arrange
     // Set up your arguments and expectations
-    const x = 4;
-    const y = 6;
-    const expected = 10;
+    const x = 11;
+    const y = 5;
+    const expected = 6;
 
     
     //2. Act 
     // Call the function you're testing and set the result to a const
-    const actual = add(x, y);// use your function here
+    const actual = subtract(x, y);// use your function here
 
     //3. Expect
     // Make assertions about what is expected versus the actual result
